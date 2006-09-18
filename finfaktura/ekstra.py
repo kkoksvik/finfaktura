3fadee79dbd75f723749ffb68e83c1870a155b89
havard.dahle <havard.dahle@64275f8e-981d-0410-8dec-e38d35af179a>
2006-09-18 00:05:56 +0000

    ekstra: QBuffer

---
+++ finfaktura/ekstra.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python -d
+# -*- coding:utf8 -*-
+###########################################################################
+#    Copyright (C) 2006 - Håvard Dahle
+#    <havard@dahle.no>
+#
+#    Lisens: GPL2
+#
+# $Id: sikkerhetskopi.py,v 1.2 2006/09/10 03:32:48 havardda Exp $
+###########################################################################
+
+
+from qt import *
+from cStringIO import StringIO
+
+class QBuffer(QIODevice):
+
+    def __init__(self):
+        QIODevice.__init__(self)
+        self.open( IO_ReadOnly)
+
+    def open(self, mode):
+        self.__io = StringIO()
+        return True
+
+    def close(self):
+        self.__io.close()
+
+    def flush(self):
+        self.__io.flush()
+
+    def readAll(self):
+        return self.__io.read()
+
+    def getch(self):
+        return self.__io.read(1)
+
+    def readBlock(self, size):
+        result = self.__io.read(size)
+        if result:
+            return (result,)
+
+        return None
+
+    def writeBlock(self, data, length=None):
+        if type(data) == QByteArray:
+            data = data.data()
+
+        self.__io.write(data)
+
+        return len(data)
+
+    def getData(self):
+        return self.__io.getvalue()
