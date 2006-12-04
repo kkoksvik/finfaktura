c31aef5e8a76415c721564bcea005f5f3cb48d2a
havard.dahle <havard.dahle@64275f8e-981d-0410-8dec-e38d35af179a>
2006-12-04 23:52:30 +0000

    legger til historikk.py for faktura-historikk

--- finfaktura/__init__.py
+++ finfaktura/__init__.py
@@ -9,7 +9,7 @@
 # $Id$
 ###########################################################################
 
-__version__ = "0.92"
+__version__ = "0.94"
 
-__all__ = ['fakturabibliotek','oppgradering','f60','cli','faktura_ui','sikkerhetskopi', 'okonomi', 'epost', 'ekstra']
+__all__ = ['fakturabibliotek','oppgradering','f60','cli','faktura_ui','sikkerhetskopi', 'okonomi', 'epost', 'historikk', 'ekstra']
 
