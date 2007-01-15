08c6e422b9355232f64a2335c3c3811ec8d3eeb6
havard.dahle <havard.dahle@64275f8e-981d-0410-8dec-e38d35af179a>
2007-01-15 23:50:09 +0000

    Gjør 0.96.2 klar til utgivelse

--- finfaktura/__init__.py
+++ finfaktura/__init__.py
@@ -9,7 +9,7 @@
 # $Id$
 ###########################################################################
 
-__version__ = "0.96"
+__version__ = "0.96.2"
 
 __all__ = ['fakturabibliotek','oppgradering','f60','cli','faktura_ui','sikkerhetskopi', 'okonomi', 'epost', 'historikk', 'ekstra']
 
