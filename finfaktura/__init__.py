164e973c68bb6a5447c2b318ea7900e9ca21041e
havard.dahle <havard.dahle@64275f8e-981d-0410-8dec-e38d35af179a>
2007-04-29 17:45:25 +0000

    bumper til 1.0!!

--- finfaktura/__init__.py
+++ finfaktura/__init__.py
@@ -1,7 +1,7 @@
 """Fakturafunksjoner for Fryktelig fin faktura"""
 # -*- coding:utf8 -*-
 ###########################################################################
-#    Copyright (C) 2006 - Håvard Dahle
+#    Copyright (C) 2006-2007 - Håvard Dahle
 #    <havard@dahle.no>
 #
 #    Lisens: GPL2
@@ -9,7 +9,7 @@
 # $Id$
 ###########################################################################
 
-__version__ = "0.96.2"
+__version__ = "1.0"
 
 __all__ = ['fakturabibliotek','oppgradering','f60','cli','faktura_ui','sikkerhetskopi', 'okonomi', 'epost', 'historikk', 'ekstra']
 
