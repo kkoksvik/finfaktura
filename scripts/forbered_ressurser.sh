91984e6bb1e91f8f3bb1eda62b97196db426ed1c
havard.gulldahl <havard.gulldahl@64275f8e-981d-0410-8dec-e38d35af179a>
2008-08-31 14:11:46 +0000

    fikser og kommenterer

--- scripts/forbered_ressurser.sh
+++ scripts/forbered_ressurser.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 # Oppdaterer alle .ui og .qrc-filer
 
+echo "Forbereder GUI (grafisk grensesnitt)";
 for f in finfaktura/ui/*.ui;
 do
   F=$(basename "$f" .ui);
+  echo ".. $F.ui";
   pyuic4 -o "finfaktura/ui/$F" "$f";
 done
 
+echo "Forbereder QRC (ressurser)";
+echo ".. faktura.qrc";
 pyrcc4 -o "finfaktura/ui/faktura_rc.py" faktura.qrc;
 
