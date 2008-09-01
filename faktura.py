4c73d5bda6ba37d0bbc320ec495e7d2d7415462f
havard.gulldahl <havard.gulldahl@64275f8e-981d-0410-8dec-e38d35af179a>
2008-09-01 22:15:38 +0000

    bedre feilmelding og rapportering

--- faktura.py
+++ faktura.py
@@ -36,13 +36,11 @@ if __name__ == "__main__":
     else:
         try:
             import finfaktura.gui
-            finfaktura.gui.start()
+            sys.exit(finfaktura.gui.start())
         except RessurserManglerFeil, (e):
             print e
-            #print u"Feil! Ressursene er ikke generert (det må du gjøre dersom du sjekker ut fra SVN)"
-            print u"Ta en titt på scripts/forbered_ressurser.sh eller setup.py"
-            #raise
-            print u"Gjr arbeidet for deg"
+            print u"Feil! Ressursene er ikke generert (det må \ndu gjøre dersom du sjekker ut fra SVN selv)"
+            print u"Lager ressursene. Ta en titt på \nscripts/forbered_ressurser.sh eller \nfinfaktura/ekstra.py for detaljer"
             from finfaktura.ekstra import forbered_ressurser
             forbered_ressurser()
         except ImportError, (e):
