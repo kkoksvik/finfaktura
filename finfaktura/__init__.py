bc0f2d8e9b2bf66699056e2c861ac4bde280eef2
havard.gulldahl <havard.gulldahl@64275f8e-981d-0410-8dec-e38d35af179a>
2008-10-12 22:31:16 +0000

    Gjør 1.9.9b2 klar til utgivelse

--- finfaktura/__init__.py
+++ finfaktura/__init__.py
@@ -9,7 +9,7 @@
 # $Id$
 ###########################################################################
 
-__version__ = "1.9.9b"
+__version__ = "1.9.9b2"
 
 __all__ = ['fakturabibliotek','oppgradering','f60','cli', 'sikkerhetskopi', 'okonomi', 'epost', 'historikk', 'ekstra', 'fakturafeil', 'fakturakomponenter', 'rapport', 'gui']
 
