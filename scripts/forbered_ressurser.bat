4e51bdf5ab4700ef25128fdc07bb5f502f3278f7
havard.gulldahl <havard.gulldahl@64275f8e-981d-0410-8dec-e38d35af179a>
2008-12-17 19:11:11 +0000

    fikser så det faktisk gjør noe

--- scripts/forbered_ressurser.bat
+++ scripts/forbered_ressurser.bat
@@ -1,18 +1,30 @@
 @ECHO OFF
 SET RESSURSER="faktura.qrc"
-SET UIDIR="..\finfaktura\ui"
-SET UIFILER="epost.ui faktura.ui finfaktura_oppsett.ui firmainfo.ui sendepost.ui"
+SET UIDIR="finfaktura\ui"
 
 ECHO Kompilerer ressurser...
-REM FOR %A IN (list) DO command [ parameters ]
-REM FOR %? IN (%RESSURSER%) DO ECHO pyrcc4 -o %UIDIR%\faktura_rc.py %?
-pyrcc4 -o %UIDIR%\faktura_rc.py ..\faktura.qrc
+pyrcc4 -o %UIDIR%\faktura_rc.py faktura.qrc
 
 ECHO Kompilerer grensesnitt...
-REM FOR %? IN (%UIFILER%) DO ECHO %? 
-pyuic4 -o %UIDIR%\epost_ui.py %UIDIR%\epost.ui
-pyuic4 -o %UIDIR%\faktura_ui.py %UIDIR%\faktura.ui
-pyuic4 -o %UIDIR%\finfaktura_oppsett_ui.py %UIDIR%\finfaktura_oppsett.ui
-pyuic4 -o %UIDIR%\firmainfo_ui.py %UIDIR%\firmainfo.ui
-pyuic4 -o %UIDIR%\sendepost_ui.py %UIDIR%\sendepost.ui
+
+ECHO faktura.ui
+
+CMD /C pyuic4 -o %UIDIR%\faktura_ui.py %UIDIR%\faktura.ui
+
+ECHO finfaktura_oppsett.ui
+
+CMD /C pyuic4 -o %UIDIR%\finfaktura_oppsett_ui.py %UIDIR%\finfaktura_oppsett.ui
+
+ECHO firmainfo.ui
+
+CMD /C pyuic4 -o %UIDIR%\firmainfo_ui.py %UIDIR%\firmainfo.ui
+
+ECHO sendepost.ui
+
+CMD /C pyuic4 -o %UIDIR%\sendepost_ui.py %UIDIR%\sendepost.ui
+
+ECHO epost.ui
+
+CMD /C pyuic4 -o %UIDIR%\epost_ui.py %UIDIR%\epost.ui
+
 
