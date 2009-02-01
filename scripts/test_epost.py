20c16db0d1d7ba077464f4c7b79fbcafdc9d0064
havard.gulldahl <havard.gulldahl@64275f8e-981d-0410-8dec-e38d35af179a>
2009-02-01 22:43:47 +0000

    legger inn testskript for epostfeil (issue#46)

---
+++ scripts/test_epost.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import smtplib, logging
+
+smtpserver = raw_input('smtp server (smtp.gmail.com): ')
+if not smtpserver: smtpserver = 'smtp.gmail.com'
+smtpport = raw_input('smtp port (587): ')
+if not smtpport: smtpport = 587
+brukernavn = raw_input('brukernavn: ')
+passord = raw_input('passord: ')
+
+s = smtplib.SMTP()
+s.set_debuglevel(3)    
+try:                       
+    s.connect(smtpserver, smtpport)
+    s.ehlo()                                 
+    s.starttls()                         
+    s.ehlo()                             
+    s.login(brukernavn, passord)
+except Exception, (e):
+    logging.exception(e)
+
+
+
