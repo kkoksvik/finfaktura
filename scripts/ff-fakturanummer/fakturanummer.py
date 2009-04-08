b28dbc5ae8b4f9143cc0c28058d1823b0993b50c
havard.gulldahl <havard.gulldahl@64275f8e-981d-0410-8dec-e38d35af179a>
2009-04-08 09:53:21 +0000

    mer'

--- scripts/ff-fakturanummer/fakturanummer.py
+++ scripts/ff-fakturanummer/fakturanummer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python -d
 # -*- coding: utf-8 -*-
-# kate: indent-width 4;
+# kate: indent-width 4; encoding utf-8
 ###########################################################################
 #    Copyright (C) 2009 Håvard Gulldahl
 #    <havard@gulldahl.no>
@@ -16,14 +16,62 @@ import sys, os, os.path, logging, glob
 from time import time, strftime, localtime, mktime
 import logging
 
+from stat import ST_MTIME
+
 from PyQt4 import QtCore, QtGui, uic
+
+import sqlite3
 
 class nummersetter(object):
   def __init__(self):
+    pass
+
+  def settDatabase(self, db):
+    self.databasenavn = db
+    self.db = sqlite3.connect(db)
+
+  def lesDBInfo(self, database=None):
+    if database is None:
+      databasenavn = self.databasenavn
+      db = self.db
+    else:
+      databasenavn = database
+      if not os.path.exists(databasenavn): return False
+      db = sqlite3.connect(databasenavn)
+
+
+    mtime = os.stat(databasenavn)[ST_MTIME]
+
+    cursor = db.cursor()
+    try:
+      cursor.execute('SELECT firmanavn FROM Firma')
+      fnavn = cursor.fetchone()
+      cursor.execute('SELECT * FROM Ordrehode')
+      ffaktura = cursor.fetchall()
+
+      status = len(ffaktura) == 0
+    except sqlite3.Error:
+      status = False
+      firmanavn = 'Feil'
+
+    
+    ret = {'filnavn':databasenavn,
+           'firmanavn':firmanavn,
+           'endret':mtime,
+           'status':status}
+
+    return ret
+    
+        
+
+
+class nummersettergui(object):
+  def __init__(self):
     self.gui = uic.loadUi('gui.ui')
-    self.gui.connect(self.gui.databasenavn, QtCore.SIGNAL('currentIndexChanged(QString&)'), self.slotDatabaseValgt)
+    self.gui.connect(self.gui.databasenavn, QtCore.SIGNAL('activated(QString)'), self.slotDatabaseValgt)
     self.gui.show()
     self.gui.databasenavn.addItems(list(self.listDatabaser()))
+    self.help = nummersetter()
 
   def listDatabaser(self):
     if os.path.exists(os.getenv('FAKTURADB', '')):
@@ -40,12 +88,19 @@ class nummersetter(object):
 
   def slotDatabaseValgt(self, s):
     logging.debug('valgte database: %s', s)
-    
+    filename = unicode(s, sys.getfilesystemencoding())
+    if filename == '...':
+      return self.velgDatabase()
+    status = self.help.lesDBInfo()
+    logging.debug('status:%s', status)
+
+  
       
 
 if __name__ == '__main__':
+  logging.basicConfig(level=logging.DEBUG)
   a = QtGui.QApplication(sys.argv)
-  p = nummersetter()
+  p = nummersettergui()
   a.exec_()
 
   
