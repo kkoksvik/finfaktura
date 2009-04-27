846e922e1a8cfa194d6814ec696c1fb35a3e4155
havard.gulldahl <havard.gulldahl@64275f8e-981d-0410-8dec-e38d35af179a>
2009-04-27 19:16:01 +0000

    mer jobbing på bug#24 -- gui tar form

--- finfaktura/gui_sendepost.py
+++ finfaktura/gui_sendepost.py
@@ -10,19 +10,33 @@
 #
 ###########################################################################
 
-from PyQt4 import QtGui
+import sys, os, logging
+from PyQt4 import QtCore, QtGui
 from ui import sendepost_ui
 
 class sendEpost(sendepost_ui.Ui_sendEpost):
+    _vedlegg = []
     def __init__(self, parent, ordre):
         self.parent = parent
-        #sendepost_ui.Ui_sendEpost.__init__()
         self.gui = QtGui.QDialog()
         self.setupUi(self.gui)
         self.tittel.setText(u'Sender faktura til %s <b>&lt;%s</b>&gt;' % (ordre.kunde.navn, ordre.kunde.epost))
         self.tekst.setPlainText(u'Vedlagt følger epostfaktura #%i:\n%s\n\n-- \n%s\n%s' % (ordre.ID, ordre.tekst,  ordre.firma, ordre.firma.vilkar))
+        self.gui.connect(self.leggVedFil, QtCore.SIGNAL("clicked()"), self.lagVedlegg)
+        self.vedlegg.hide()
         self.gui.show()
 
+    def lagVedlegg(self):
+        f = QtGui.QFileDialog.getOpenFileName(self.gui,
+            u"Velg en fil å legge ved",
+            os.getenv('HOME', '.'))
+        if len(f) > 0:
+            self.vedlegg.show()
+            logging.debug("Legger ved fil: %s", unicode(f))
+            self._vedlegg.append(unicode(f).encode(sys.getfilesystemencoding()))
+            i = QtGui.QTreeWidgetItem([f, '', ''])
+            self.vedlegg.addTopLevelItem(i)
+            
     def exec_(self):
         res = self.gui.exec_()
         return res, unicode(self.tekst.toPlainText())
